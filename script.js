e8a922cca7cac0a5782d74d3a5c322139ba2c205
Marcin Żebrowski <mzebrowski@amplfier.pl>
2022-05-22 13:41:27 +0200

    changing content

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let czekolada = document.getElementById("czekolada");
-let wisnia = document.getElementById("wisnia");
+let dynia = document.getElementById("dynia");
+let cytryna = document.getElementById("cytryna");
 let candle = document.getElementById("candle");
 
-czekolada.onclick = function () {
+dynia.onclick = function () {
     candle.style.backgroundImage = "url(babka1.png)";
+    dynia.classList.add('active');
+    cytryna.classList.remove('active');
 }
 
-wisnia.onclick = function () {
+cytryna.onclick = function () {
     candle.style.backgroundImage = "url(babka2.png)";
+    cytryna.classList.add('active');
+    dynia.classList.remove('active');
 }
