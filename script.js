0e09cea59017ad1a3e956074781a91d7a5aab44e
Marcin Żebrowski <mzebrowski@amplfier.pl>
2022-05-22 20:04:25 +0200

    a

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ cytryna.onclick = function () {
     cytryna.classList.add('active');
     dynia.classList.remove('active');
 }
+
+$(document).ready(function () {
+    $('.your-class').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+});
