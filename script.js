0a4a94d8ea2ad1fcff9fd4c49652558ac01b790f
Marcin Żebrowski <mzebrowski@amplfier.pl>
2022-05-23 13:00:48 +0200

    ok

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let cytryna = document.getElementById("cytryna");
 let candle = document.getElementById("candle");
 
 dynia.onclick = function () {
-    candle.style.backgroundImage = "url(babka1.png)";
+    candle.style.backgroundImage = "url(Cupcake_pumpkin.png)";
     dynia.classList.add('active');
     cytryna.classList.remove('active');
 }
 
 cytryna.onclick = function () {
-    candle.style.backgroundImage = "url(babka2.png)";
+    candle.style.backgroundImage = "url(Cupcake_lemon.png)";
     cytryna.classList.add('active');
     dynia.classList.remove('active');
 }
