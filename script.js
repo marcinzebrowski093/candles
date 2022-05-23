3f45ca8617d99110ec51d504059cd6783c08886f
Marcin Żebrowski <mzebrowski@amplfier.pl>
2022-05-23 13:14:00 +0200

    ok

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 let dynia = document.getElementById("dynia");
 let cytryna = document.getElementById("cytryna");
 let candle = document.getElementById("candle");
+let x = window.matchMedia("(min-width:1024px)");
 
-dynia.onclick = function () {
-    candle.style.backgroundImage = "url(Cupcake_pumpkin.png)";
-    dynia.classList.add('active');
-    cytryna.classList.remove('active');
+if (x.matches) {
+    dynia.onclick = function () {
+        candle.style.backgroundImage = "url(Cupcake_pumpkin.png)";
+        dynia.classList.add('active');
+        cytryna.classList.remove('active');
+    }
 }
 
-cytryna.onclick = function () {
-    candle.style.backgroundImage = "url(Cupcake_lemon.png)";
-    cytryna.classList.add('active');
-    dynia.classList.remove('active');
+if (x.matches) {
+
+    cytryna.onclick = function () {
+        candle.style.backgroundImage = "url(Cupcake_lemon.png)";
+        cytryna.classList.add('active');
+        dynia.classList.remove('active');
+    }
+}
+
+if (!x.matches) {
+    dynia.onclick = function () {
+        candle.style.backgroundImage = "url(mobile_pumpkin.png)";
+        dynia.classList.add('active');
+        cytryna.classList.remove('active');
+    }
+}
+
+if (!x.matches) {
+    cytryna.onclick = function () {
+        candle.style.backgroundImage = "url(mobile_lemon.png)";
+        cytryna.classList.add('active');
+        dynia.classList.remove('active');
+    }
 }
